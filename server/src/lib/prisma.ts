7b6d1901d06a533a10289c6b407cf19ff7b42908
random-ization <ssunhr@gmail.com>
2025-12-07 06:02:00 +0900

    Add adapter configuration to PrismaClient instantiation

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,9 +1,11 @@
 // @ts-ignore
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, defineAdapter } from '@prisma/client'
 
 // Prevent multiple instances in development due to hot reloading
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter: defineAdapter({ url: process.env.DATABASE_URL })
+});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
